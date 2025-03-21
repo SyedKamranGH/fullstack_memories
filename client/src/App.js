f2e5517c45800deff09dbfd84ca5c009a509610c
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-03-22 04:22:32 +0500

    Basic UI introduction

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
+// import Container from "@mui/material/Container";
+import { AppBar, Container, Grid2, Grow, Typography } from "@mui/material";
+import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">App</header>
-    </div>
+    <Container className="App" maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid2
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid2 item xs={12} sm={7}>
+              <Posts />
+            </Grid2>
+            <Grid2 item xs={12} sm={4}>
+              <Form />
+            </Grid2>
+          </Grid2>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
